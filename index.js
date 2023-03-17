9d32b2c6203562686a1c918302c0fee3230484d5
Matteo Ceserani <ceserani@gmail.com>
2023-03-17 22:37:34 +0100

    GPT4 Solution

--- index.js
+++ index.js
@@ -40,25 +40,110 @@ function main() {
 
 function addEvent() {
   
-    // SCRIVI IL TUO CODICE QUI
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Titolo dell\'evento:',
+      },
+      {
+        type: 'input',
+        name: 'dateTime',
+        message: 'Data e ora dell\'evento (es. 01/01/2023 18:00):',
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Luogo dell\'evento:',
+      },
+      {
+        type: 'number',
+        name: 'maxTickets',
+        message: 'Numero massimo di biglietti disponibili:',
+      },
+    ])
+    .then((event) => {
+      event.bookedTickets = 0;
+      events.push(event);
+      console.log('Evento aggiunto con successo!');
+      main();
+    });
 
 }
 
 function viewEvents() {
+events.forEach((event, index) => {
+    console.log(`\nEvento ${index + 1}:`);
+    console.log(`Titolo: ${event.title}`);
+    console.log(`Data e ora: ${event.dateTime}`);
+    console.log(`Luogo: ${event.location}`);
+    console.log(`Biglietti disponibili: ${event.maxTickets - event.bookedTickets}`);
+  });
 
-    // SCRIVI IL TUO CODICE QUI
-
+  main();
 }
 
 function bookTickets() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'eventToBook',
+        message: 'Per quale evento vuoi prenotare biglietti?',
+        choices: events.map((event, index) => `${index + 1}. ${event.title}`),
+      },
+      {
+        type: 'number',
+        name: 'tickets',
+        message: 'Quanti biglietti vuoi prenotare?',
+      },
+    ])
+    .then((answers) => {
+      const eventIndex = parseInt(answers.eventToBook.split('.')[0]) - 1;
+      const event = events[eventIndex];
+      const availableTickets = event.maxTickets - event.bookedTickets;
 
-    // SCRIVI IL TUO CODICE QUI
+      if (answers.tickets <= availableTickets) {
+        event.bookedTickets += answers.tickets;
+        console.log('Biglietti prenotati con successo!');
+      } else {
+        console.log('Biglietti insufficienti. Riprova.');
+      }
 
+      main();
+    });
 }
 
 function cancelBooking() {
-    
-    // SCRIVI IL TUO CODICE QUI
+    inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'eventToCancel',
+        message: 'Per quale evento vuoi cancellare la prenotazione?',
+        choices: events.map((event, index) => `${index + 1}. ${event.title}`),
+},
+{
+type: 'number',
+name: 'tickets',
+message: 'Quanti biglietti vuoi cancellare?',
+},
+])
+.then((answers) => {
+const eventIndex = parseInt(answers.eventToCancel.split('.')[0]) - 1;
+const event = events[eventIndex];
+  if (answers.tickets <= event.bookedTickets) {
+    event.bookedTickets -= answers.tickets;
+    console.log('Prenotazione cancellata con successo!');
+  } else {
+    console.log(
+      'Il numero di biglietti da cancellare è superiore a quelli prenotati. Riprova.'
+    );
+  }
+
+  main();
+});
 
 }
 

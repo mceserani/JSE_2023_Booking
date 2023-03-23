9935be059ea4b5c2649ac1bcafe974d325a78ac5
Matteo Ceserani <ceserani@gmail.com>
2023-03-23 20:31:48 +0100

    Completata

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import inquirer from "inquirer";
 const events = [];
 
 function main() {
-  inquirer
-    .prompt([
+  inquirer.prompt([
       {
         type: 'list',
         name: 'action',
@@ -39,27 +38,135 @@ function main() {
 }
 
 function addEvent() {
-  
-    // SCRIVI IL TUO CODICE QUI
-
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'titolo',
+            message: 'Inserisci il titolo dell\'evento',
+            validate: (value) => {
+                if (value.length > 0) {
+                    return true;
+                }
+                return 'Inserisci un titolo';
+            }
+        },
+        {
+            type: 'input',
+            name: 'datetime',
+            message: 'Inserisci la data e l\'ora dell\'evento',
+            validate: (value) => {
+                if (value.length > 0 && value.match(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4} [0-9]{2}:[0-9]{2}$/)) {
+                    return true;
+                }
+                return 'Inserisci una data e un\'ora nel formato gg/mm/aaaa hh:mm';
+            }
+        },
+        {
+            type: 'input',
+            name: 'luogo',
+            message: 'Inserisci il luogo dell\'evento',
+            validate: (value) => {
+                if (value.length > 0) {
+                    return true;
+                }
+                return 'Inserisci un luogo';
+            }
+        },
+        {
+            type: 'input',
+            name: 'posti',
+            message: 'Inserisci il numero di posti disponibili',
+            validate: (value) => {
+                if (value.length > 0 && value.match(/^[0-9]+$/)) {
+                    return true;
+                }
+                return 'Inserisci un numero di posti valido';
+            }
+        }
+    ]).then((event) => {
+        event.prenotati = 0;
+        events.push(event);
+        console.log('Evento aggiunto');
+        main();
+    });
 }
 
 function viewEvents() {
-
-    // SCRIVI IL TUO CODICE QUI
-
+    for (let event of events){
+        console.log(`Titolo: ${event.titolo}`);
+        console.log(`Data e ora: ${event.datetime}`);
+        console.log(`Luogo: ${event.luogo}`);
+        console.log(`Posti: ${event.posti}`);
+        console.log(`Posti prenotati: ${event.prenotati}`);
+    }
+    main();
 }
 
 function bookTickets() {
-
-    // SCRIVI IL TUO CODICE QUI
-
+    let choices = events.map((event, index) => {
+        let disponibili = Number(event.posti) - Number(event.prenotati);
+        return `${index + 1}: ${event.titolo} - ${disponibili}`;
+    });
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'eventToBook',
+            message: 'Scegli l\'evento per cui vuoi prenotare',
+            choices: choices
+        },
+        {
+            type: 'input',
+            name: 'howmany',
+            message: 'Quanti posti vuoi prenotare',
+            validate: (value) => {
+                if (value.length > 0 && value.match(/^[0-9]+$/))
+                    return true;
+                else
+                    return 'Inserisci un numero di posti valido'
+            }
+        }
+    ]).then((answers) => {
+        let index = Number(answers.eventToBook.split(':')[0]) - 1;
+        if (Number(events[index].posti) - events[index].prenotati >= Number(answers.howmany)){
+            events[index].prenotati += Number(answers.howmany);
+            console.log("Prenotazione effettuata!");
+        }else
+            console.log("Errore!");
+        main(); 
+    });
 }
 
 function cancelBooking() {
-    
-    // SCRIVI IL TUO CODICE QUI
-
+    let choices = events.map((event, index) => {
+        return `${index + 1}: ${event.titolo} - ${Number(event.prenotati)}`;
+    });
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'eventToBook',
+            message: 'Scegli l\'evento per cui vuoi disdire',
+            choices: choices
+        },
+        {
+            type: 'input',
+            name: 'howmany',
+            message: 'Quanti posti vuoi disdire',
+            validate: (value) => {
+                if (value.length > 0 && value.match(/^[0-9]+$/))
+                    return true;
+                else
+                    return 'Inserisci un numero di posti valido'
+            }
+        }
+    ]).then((answers) => {
+        let index = Number(answers.eventToBook.split(':')[0]) - 1;
+        if (Number(events[index].prenotati) >= Number(answers.howmany)){
+            events[index].prenotati -= Number(answers.howmany);
+            console.log("Disdetta effettuata!");
+        }else
+            console.log("Errore!");
+        main(); 
+    });
 }
 
 main();
